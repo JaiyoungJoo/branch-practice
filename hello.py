e419cf2bd6ce3b7817832d1c408095474de59378
{JaiyoungJoo} <{wodudwn@daum.net}>
2022-05-06 15:15:31 +0900

    메인파일임

--- hello.py
+++ hello.py
@@ -1,9 +1,13 @@
-for i in range(1, 16+1):
+for i in range(1, 16for i in range(1, 16+1):
     if i % 15 == 0:
         print('yeardream')
     elif i % 5 == 0:
         print('dream')
     else:
         print(i)
+
+
+
+
     
     #뭐 이상한거 했나
